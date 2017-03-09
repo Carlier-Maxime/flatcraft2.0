93b5b8630e9c0297f5d0dd72beca603e74e2ca2f
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2017-03-09 20:59:57 +0100

    ajout de l'analyse sonar

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+find src -name *.java -print >javafiles
+if [ ! -d bin ]; then
+    mkdir bin
+fi
+javac -d bin @javafiles
+cp -R src/textures bin
+jar -cvfm flatcraft.jar manifest.mf -C bin/ .
