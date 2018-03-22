a75f3b233d0569d281548f8f19afbe72af6aa429
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2018-03-22 22:51:43 +0100

    Probably fixed the first issue with the furnace (see #8)

--- src/dut/flatcraft/CombustibleContainer.java
+++ src/dut/flatcraft/CombustibleContainer.java
@@ -8,8 +8,8 @@ public class CombustibleContainer extends ResourceContainer {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	public static final DataFlavor COMBUSTIBLE_FLAVOR = new DataFlavor(ResourceContainer.class, "resourcecontainer");
+
+	public static final DataFlavor COMBUSTIBLE_FLAVOR = new DataFlavor(CombustibleContainer.class, "combustibleflavor");
 
 	public CombustibleContainer(Resource block, int quantity) {
 		super(block, quantity);
@@ -17,14 +17,14 @@ public class CombustibleContainer extends ResourceContainer {
 
 	@Override
 	public DataFlavor[] getTransferDataFlavors() {
-		return new DataFlavor[] {COMBUSTIBLE_FLAVOR };
+		return new DataFlavor[] { COMBUSTIBLE_FLAVOR, RESOURCE_FLAVOR };
 	}
 
 	@Override
 	public boolean isDataFlavorSupported(DataFlavor flavor) {
 		return COMBUSTIBLE_FLAVOR.equals(flavor);
 	}
-	
+
 	@Override
 	public CombustibleContainer clone() {
 		return new CombustibleContainer(getResource(), getQuantity());
