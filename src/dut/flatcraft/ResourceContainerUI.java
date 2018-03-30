3164f7bf546a8829779e310e14568035e6010ce6
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2018-03-30 09:11:52 +0200

    Potential fix for #18

--- src/dut/flatcraft/ResourceContainerUI.java
+++ src/dut/flatcraft/ResourceContainerUI.java
@@ -11,57 +11,57 @@ import javax.swing.JButton;
 
 public class ResourceContainerUI extends JButton implements ResourceContainerListener, HandableUI {
 
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
-    private final ResourceContainer container;
+	private final ResourceContainer container;
 
-    public ResourceContainerUI(Resource block) {
-        this(block, 0);
-    }
+	public ResourceContainerUI(Resource block) {
+		this(block, 0);
+	}
 
-    public ResourceContainerUI(Resource block, int quantity) {
-        this(new ResourceContainer(block, quantity));
-    }
+	public ResourceContainerUI(Resource block, int quantity) {
+		this(new ResourceContainer(block, quantity));
+	}
 
-    public ResourceContainerUI(ResourceContainer container) {
-        this.container = container;
-        container.addListener(this);
-        this.setIcon(container.getImage());
-        setPreferredSize(new Dimension(MineUtils.DEFAULT_IMAGE_SIZE, MineUtils.DEFAULT_IMAGE_SIZE));
-        setBorder(BorderFactory.createEmptyBorder());
-        setToolTipText(container.getResource().getName());
-        setFocusable(false);
-    }
+	public ResourceContainerUI(ResourceContainer container) {
+		this.container = container;
+		container.addListener(this);
+		this.setIcon(container.getImage());
+		setPreferredSize(new Dimension(MineUtils.DEFAULT_IMAGE_SIZE, MineUtils.DEFAULT_IMAGE_SIZE));
+		setBorder(BorderFactory.createEmptyBorder());
+		setToolTipText(container.getResource().getName());
+		setFocusable(false);
+	}
 
-    public ResourceContainer getResourceContainer() {
-        return container;
-    }
+	public ResourceContainer getResourceContainer() {
+		return container;
+	}
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        int qty = container.getQuantity();
-        if (qty > 0) {
-            super.paintComponent(g);
-            Rectangle rect = g.getClipBounds();
-            g.setColor(Color.YELLOW);
-            g.fillOval(rect.x +  20, rect.y + 20, 20, 20);
-            g.setColor(Color.BLACK);
-            g.setFont(g.getFont().deriveFont(Font.BOLD, g.getFont().getSize()));
-            if (qty < 10) {
-                g.drawString(String.valueOf(qty), rect.x + 27, rect.y + 34);
-            } else {
-                g.drawString(String.valueOf(qty), rect.x + 23, rect.y + 34);
-            }
-        }
-    }
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		int qty = container.getQuantity();
+		if (qty > 0) {
+			Rectangle rect = g.getClipBounds();
+			g.setColor(Color.YELLOW);
+			g.fillOval(rect.x + 20, rect.y + 20, 20, 20);
+			g.setColor(Color.BLACK);
+			g.setFont(g.getFont().deriveFont(Font.BOLD, g.getFont().getSize()));
+			if (qty < 10) {
+				g.drawString(String.valueOf(qty), rect.x + 27, rect.y + 34);
+			} else {
+				g.drawString(String.valueOf(qty), rect.x + 23, rect.y + 34);
+			}
+		}
+	}
 
-    @Override
-    public void update(ResourceContainer source) {
-        repaint();
-    }
+	@Override
+	public void update(ResourceContainer source) {
+		repaint();
+	}
 
 	@Override
 	public Handable getHandable() {
