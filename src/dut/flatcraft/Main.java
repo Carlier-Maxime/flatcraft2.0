fdcc131100a8840bcd74104bd5f58682e6f5e5eb
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2019-03-01 20:31:46 +0100

    Potential fix for #21.

--- src/dut/flatcraft/Main.java
+++ src/dut/flatcraft/Main.java
@@ -1,6 +1,8 @@
 package dut.flatcraft;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -9,39 +11,42 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-		MyGrid grid = new MyGrid(19,120,new ResourceCellFactory(), new TerrilDecorator(new TreeDecorator(new SimpleGenerator(),10,5),5));
-		JScrollPane scrollpane = new JScrollPane(grid,JScrollPane.VERTICAL_SCROLLBAR_NEVER,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+		MyGrid grid = new MyGrid((dimension.height - 60) / 40, 120, new ResourceCellFactory(),
+				new TerrilDecorator(new TreeDecorator(new SimpleGenerator(), 10, 5), 5));
+		JScrollPane scrollpane = new JScrollPane(grid, JScrollPane.VERTICAL_SCROLLBAR_NEVER,
+				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollpane.getVerticalScrollBar().setUnitIncrement(40);
 		scrollpane.getHorizontalScrollBar().setUnitIncrement(40);
-	    scrollpane.setDoubleBuffered(true);
-		frame.add(BorderLayout.CENTER,scrollpane);
+		scrollpane.setDoubleBuffered(true);
+		frame.add(BorderLayout.CENTER, scrollpane);
 		JPanel south = new JPanel();
 		south.setLayout(new BorderLayout());
-		
+
 		JDialog craft = new JDialog(frame, "Craft Table");
 		craft.add(new CraftTable(grid.getPlayer()));
 		craft.pack();
-        
+
 		JButton craftButton = new JButton("Craft");
 		craftButton.addActionListener(e -> craft.setVisible(true));
 		craftButton.setFocusable(false);
-		
+
 		south.add(BorderLayout.WEST, craftButton);
 		south.add(BorderLayout.CENTER, grid.getPlayer().getInventoryUI());
-		
+
 		JDialog cook = new JDialog(frame, "Furnace");
 		cook.add(new Furnace(grid.getPlayer()));
 		cook.pack();
-		
+
 		JButton cookButton = new JButton("Cook");
 		cookButton.addActionListener(e -> cook.setVisible(true));
 		cookButton.setFocusable(false);
-		
+
 		south.add(BorderLayout.EAST, cookButton);
-		frame.add(BorderLayout.SOUTH,south);
+		frame.add(BorderLayout.SOUTH, south);
 		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
