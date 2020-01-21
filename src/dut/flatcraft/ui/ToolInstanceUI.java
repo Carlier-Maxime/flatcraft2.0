8f0895dc2530391041cc317bdac3be64897c72e7
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2020-01-21 22:38:15 +0100

    Fix for #7

--- src/dut/flatcraft/ui/ToolInstanceUI.java
+++ src/dut/flatcraft/ui/ToolInstanceUI.java
@@ -3,6 +3,7 @@ package dut.flatcraft.ui;
 import static dut.flatcraft.MineUtils.DEFAULT_IMAGE_SIZE;
 
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Rectangle;
@@ -37,11 +38,11 @@ public class ToolInstanceUI extends JButton implements ToolInstanceListener, Inv
 
 	@Override
 	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Rectangle rect = g.getClipBounds();
+		g.setColor(Color.GRAY);
+		g.fillRect(rect.x + 5, rect.y + DEFAULT_IMAGE_SIZE / 2, DEFAULT_IMAGE_SIZE - 10, 3);
 		if (tool.getCurrentLife() > 0) {
-			super.paintComponent(g);
-			Rectangle rect = g.getClipBounds();
-			g.setColor(Color.GRAY);
-			g.fillRect(rect.x + 5, rect.y + DEFAULT_IMAGE_SIZE / 2, DEFAULT_IMAGE_SIZE - 10, 3);
 			g.setColor(Color.YELLOW);
 			g.fillRect(rect.x + 5, DEFAULT_IMAGE_SIZE / 2,
 					(tool.getCurrentLife() * DEFAULT_IMAGE_SIZE - 10) / tool.getType().getInitialLife(), 3);
@@ -50,7 +51,15 @@ public class ToolInstanceUI extends JButton implements ToolInstanceListener, Inv
 
 	@Override
 	public void update(ToolInstance source) {
-		repaint();
+		if (tool.getCurrentLife() == 0) {
+			Container parent = this.getParent();
+			parent.remove(this);
+			this.setTransferHandler(null);
+			parent.invalidate();
+			parent.repaint();
+		} else {
+			repaint();
+		}
 	}
 
 	@Override
